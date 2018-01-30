858ccd6aa0769b925af7b730b4f6e8798f94bf6d
Toai Nguyen <toainguyen@Toais-MBP.net.fpt>
2018-01-30 22:55:35 +0700

    ...

---
+++ demo/application/Controllers/Demo.php
@@ -0,0 +1,23 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Log\Logger;
+use HoneyDT\FATheme\Libraries\FATheme;
+
+class Demo extends Controller
+{
+    protected $_theme;
+
+    public function __construct(RequestInterface $request, ResponseInterface $response, Logger $logger = null)
+    {
+        parent::__construct($request, $response, $logger);
+        $this->_theme = new FATheme;
+    }
+
+    public function index()
+    {
+        echo $this->_theme->load('content');
+    }
+}
